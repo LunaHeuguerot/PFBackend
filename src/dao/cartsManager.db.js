db18d5a42fe2497acf2bcf0d066e6e46062b235d
Luna Heuguerot <lunaheuguerot@gmail.com>
2024-05-29 23:19:54 -0300

    sigue problema

--- src/dao/cartsManager.db.js
+++ src/dao/cartsManager.db.js
@@ -1,26 +1,25 @@
-import mongoose from 'mongoose';
 import cartModel from './models/cart.model.js';
 import { ProductManagerDB } from './productsManager.db.js';
 
 export class CartsManagerDB {
     static #instance;
 
-    constructor() { }
+    constructor() { };
 
     static getInstance() {
-        if (!CartsManagerDB.#instance) {
+        if(!CartsManagerDB.#instance) {
             CartsManagerDB.#instance = new CartsManagerDB();
         }
         return CartsManagerDB.#instance;
-    }
+    };
 
     async getCartById(id) {
         try {
             if (id.length !== 24) {
-                throw new Error('El id debe tener 24 caracteres');
+                throw new Error ('El id debe tener 24 caracteres')
             }
 
-            const cart = await cartModel.findById(id).populate('products.productId');
+            const cart = await cartModel.findOne({ _id: id });
 
             if (!cart) {
                 throw new Error(`No se encontró el carrito con id ${id}`);
@@ -30,44 +29,40 @@ export class CartsManagerDB {
         } catch (error) {
             throw error;
         }
-    }
+    };
 
     async createCart() {
         try {
             const cart = await cartModel.create({});
 
-            if (!cart) {
-                throw new Error('No se pudo crear el carrito');
+            if(!cart) {
+                throw new Error ('No se pudo crear el carrito');
             }
 
             return cart;
         } catch (error) {
             throw error;
         }
-    }
+    };
 
-    async addProductToCart(cid, pid, quantity = 1) {
+    async addProductToCart(cartId, productId){
         try {
-            await ProductManagerDB.getInstance().getProductById(pid);
-            let cart = await this.getCartById(cid);
+            await ProductManagerDB.getInstance().getProductById(productId);
+            let cart = await this.getCartById(cartId);
+            const productIndex = cart.products.findIndex(product => product.productId === productId);
 
-            // Comparar correctamente los IDs de los productos
-            const productIndex = cart.products.findIndex(product => product.productId.toString() === pid.toString());
-
-            if (productIndex !== -1) {
-                // Incrementar la cantidad del producto existente
-                cart.products[productIndex].quantity += quantity;
+            if(productIndex !== -1){
+                cart.products[productIndex].quantity++;
             } else {
-                // Añadir el nuevo producto al carrito
-                cart.products.push({ productId: new mongoose.Types.ObjectId(pid), quantity: quantity });
+                cart.products.push({ productId: productId, quantity: 1});
             }
-
-            cart = await cartModel.findByIdAndUpdate(cid, { products: cart.products }, { new: true }).populate('products.productId');
+            cart = await cartModel.findByIdAndUpdate(cartId, { products: cart.products }, { new: true });
             return cart;
         } catch (error) {
             throw error;
         }
     }
+
 
     async updateCart(id, products) {
         try {
@@ -87,65 +82,65 @@ export class CartsManagerDB {
                 if (productIndex !== -1) {
                     cart.products[productIndex].quantity = product.quantity;
                 } else {
-                    cart.products.push({ productId: new mongoose.Types.ObjectId(product.productId), quantity: product.quantity });
+                    cart.products.push({ product: product.productId, quantity: product.quantity });
                 }
             });
 
-            await cartModel.updateOne({ _id: id }, { products: cart.products });
+            await cartModel.updateOne({ _id:id }, { products: cart.products });
 
-            cart = await this.getCartById(id).populate('products.productId');
+            cart = await this.getCartById(id);
             return cart;
         } catch (error) {
             throw error;
         }
-    }
+    };
 
     async updateProdQuantity(cartId, productId, quantity) {
         try {
             let cart = await this.getCartById(cartId);
-            const productIndex = cart.products.findIndex(product => product.productId.toString() === productId.toString());
-            if (productIndex === -1) {
+            const productIndex = cart.products.findIndex(product => product.productId.id.toString() === productId);
+            if(productIndex === -1) {
                 throw new Error(`No se encontró el producto con id ${productId} en el carrito con id ${cartId}`);
             } else {
                 cart.products[productIndex].quantity = quantity;
             }
 
             await cartModel.updateOne({ _id: cartId }, { products: cart.products });
-            cart = await this.getCartById(cartId).populate('products.productId');
+            cart = await this.getCartById(cartId);
             return cart;
         } catch (error) {
             throw error;
         }
-    }
+    };
 
     async deleteCart(id) {
         try {
             let cart = await this.getCartById(id);
             await cartModel.updateOne({ _id: id }, { products: [] });
 
-            cart = await this.getCartById(id).populate('products.productId');
+            cart = await this.getCartById(id);
             return cart;
         } catch (error) {
             throw error;
         }
-    }
+    };
 
     async removeProdFromCart(cartId, productId) {
         try {
             let cart = await this.getCartById(cartId);
-            const productIndex = cart.products.findIndex(product => product.productId.toString() === productId.toString());
+            const productIndex = cart.products.findIndex(product => product.productId.id.toString() === productId);
 
-            if (productIndex === -1) {
+            if(productIndex === -1) {
                 throw new Error(`No se encontró el producto con id ${productId} en el carrito con id ${cartId}`);
             } else {
                 cart.products.splice(productIndex, 1);
             }
 
             await cartModel.updateOne({ _id: cartId }, { products: cart.products });
-            cart = await this.getCartById(cartId).populate('products.productId');
+            cart = await this.getCartById(cartId);
             return cart;
         } catch (error) {
             throw error;
         }
-    }
+    };
 }
