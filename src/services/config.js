3e4069e01403865410de1b3b7e25529ac0bbca13
Luna Heuguerot <lunaheuguerot@gmail.com>
2024-07-08 17:52:45 -0300

    entrega 8/7

--- src/services/config.js
+++ src/services/config.js
@@ -11,10 +11,12 @@ commandLine
 commandLine.parse();
 const clOptions = commandLine.opts();
 
+const __dirname = path.resolve(path.dirname(''));
+
 const config = {
     SERVER: 'local',
     PORT: process.env.PORT || clOptions.port || 8080,
-    DIRNAME: path.dirname(new URL(import.meta.url).pathname.replace(/^\/([A-Za-z]:\/)/, '$1')), // Win
+    DIRNAME: path.join(__dirname, 'src'), // path.dirname(new URL(import.meta.url).pathname.replace(/^\/([A-Za-z]:\/)/, '$1')), // Win
     get UPLOAD_DIR() { return `${this.DIRNAME}/public/img` },
     // MONGODB_URI: 'mongodb+srv://dblunah:coderhouse123@cluster0.lnkenam.mongodb.net/', //local 
     MONGODB_URI: process.env.MONGODB_URI, //remoto
